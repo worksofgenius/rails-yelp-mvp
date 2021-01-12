0d56f7be0c07288b2c5dfba6bd829cb47c79347a
Marisa Cassidy <marisancassidy@gmail.com>
2021-01-12 23:21:44 +0900

    Fixed style issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# restaurants = Restaurant.create({ name: }, { address: }, {category: })
-#  Review.create(content: , rating: )
